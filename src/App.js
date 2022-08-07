b7d5084af9e938355501926db326d1dbc34eb2e6
Tao. H <hamzatdestiny@gmail.com>
2022-08-07 04:29:57 +0100

    Add and style search bar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 import play from './assets/icons/play_btn.svg';
 import heroImage from './assets/images/hero_image.svg';
 import heroImageVector from './assets/images/hero_image_vector.svg';
+import search from './assets/icons/search.svg';
 
 function App() {
     const openBurger = () => {
         document.getElementById('hamburger').classList.toggle('open');
         document.getElementById('topmenu').classList.toggle('open');
     }
+
     return (
         <div className="App">
             <nav id='topmenu' className='navbar'>
@@ -37,7 +39,7 @@ function App() {
                         <div className='more_info'>
                             <button><a href='#learn-more'>Learn More</a></button>
                             <button className='play_button'>
-                                <img src={play} alt='watch video' />
+                                <a href='learn-more'><img src={play} alt='watch video' /></a>
                             </button>
                         </div>
                     </div>
@@ -46,7 +48,69 @@ function App() {
                         <img className='hero_image_vector' src={heroImageVector} alt='affordable beautiful homes' />
                     </div>
                 </div>
+                <div id='search'>
+                    <div className='search_container'>
+                        <div>
+                            <select id='location' name='location'>
+                                <option>Location</option>
+                                <option>Jahi</option>
+                                <option>Maitama</option>
+                            </select>
+                        </div>
+                        <div>
+                            <select id='type' name='type'>
+                                <option>Property Type</option>
+                                <option>Duplex</option>
+                                <option>Bungalow</option>
+                            </select>
+                        </div>
+                        <div>
+                            <select id='price' name='price'>
+                                <option>Max Price</option>
+                                <option>$20,000</option>
+                                <option>200,000</option>
+                            </select>
+                        </div>
+                        <button>
+                            <img src={search} alt='search icon' />
+                            <span>Search</span>
+                        </button>
+                    </div>
+                </div>
             </main>
+            <footer id='footer'>
+                <div className='description'>
+                    <h2>Dee's Estate</h2>
+                    <p>Solution for easy and flexible house hunting. You can trust us anywhere through this platform.</p>
+                </div>
+                <div className='property information'>
+                    <h2>Property</h2>
+                    <ul>
+                        <li><a href='#learning-module'>Learning Module</a></li>
+                        <li><a href='#partnershp'>Partnership</a></li>
+                        <li><a href='#demo'>Watch demo</a></li>
+                        <li><a href='#event'>Event</a></li>
+                    </ul>
+                </div>
+                <div className='information about'>
+                    <h2>About</h2>
+                    <ul>
+                        <li><a href='#company'>Our Company</a></li>
+                        <li><a href='#career'>Career</a></li>
+                        <li><a href='#investor-relations'>Investor Relations</a></li>
+                        <li><a href='#social-impact'>Social Impact</a></li>
+                    </ul>
+                </div>
+                <div className='information resources'>
+                    <h2>Resources</h2>
+                    <ul>
+                        <li><a href='#contact'>Contact</a></li>
+                        <li><a href='#feedback'>Give Feedback</a></li>
+                        <li><a href='#status'>System Status</a></li>
+                        <li><a href='#privacy'>Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </footer>
         </div>
     )
 }
