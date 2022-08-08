bcd746d0dacd4541f161c13e01e6be7f7dce3c8b
Tao. H <hamzatdestiny@gmail.com>
2022-08-08 05:11:41 +0100

    Complete layout and styling (mvp).

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ import search from './assets/icons/search.svg';
 import addressMark from './assets/images/address_mark.png';
 import secureImg from './assets/images/secure_data_protection_with_fingerprint_and_lock.png';
 import smartHouse from './assets/images/smart_house.png';
+import whyUs from './assets/images/why_us.svg';
+import checkMark from './assets/icons/check_mark.svg';
+import thePalace from './assets/images/the_palace.svg';
+import ivyRory from './assets/images/ivy_rory.svg';
+import rexona from './assets/images/rexona.svg';
 
 function App() {
-    const openBurger = () => {
+    const openMenu = () => {
         document.getElementById('hamburger').classList.toggle('open');
         document.getElementById('topmenu').classList.toggle('open');
     }
@@ -20,66 +25,67 @@ function App() {
         <div className="App">
             <nav id='topmenu' className='navbar'>
                 <h1>Dee's Estate</h1>
-                <div id='hamburger' className='' onClick={openBurger}>
+                <div id='hamburger' className='' onClick={openMenu}>
                     <div className='top'></div>
                     <div className='mid'></div>
                     <div className='bottom'></div>
                 </div>
                 <ul className='nav-links'>
                     <li><a href='#home' className='active'>Home</a></li>
-                    <li><a href='#rent'>Rent</a></li>
-                    <li><a href='#land'>Land</a></li>
-                    <li><a href='#agent'>Agent</a></li>
-                    <li><a href='#contact'>Contact Us</a></li>
+                    <li><a href='#home'>Rent</a></li>
+                    <li><a href='#featured_properties'>Land</a></li>
+                    <li><a href='#footer'>Agent</a></li>
+                    <li><a href='#how_it_works'>Contact Us</a></li>
                 </ul>
                 <button><a href='#get-started'>Get Started</a></button>
             </nav>
-            <header id='home'>
-                <div className='intro'>
-                    <h2>Helping you find the property of your dreams.</h2>
-                    <p>Creating quality urban lifestyles, building stronger communities.</p>
-                    <div className='more_info'>
-                        <button><a href='#learn-more'>Learn More</a></button>
-                        <button className='play_button'>
-                            <a href='learn-more'><img src={play} alt='watch video' /></a>
+            <main>
+                <header id='home'>
+                    <div className='intro'>
+                        <h2>Helping you find the property of your dreams.</h2>
+                        <p>Creating quality urban lifestyles, building stronger communities.</p>
+                        <div className='more_info'>
+                            <button><a href='#learn-more'>Learn More</a></button>
+                            <button className='play_button'>
+                                <a href='learn-more'><img src={play} alt='watch video' /></a>
+                            </button>
+                        </div>
+                    </div>
+                    <div className='hero'>
+                        <img className='hero_image' src={heroImage} alt='beautiful home with pool just under dim blue sky' />
+                        <img className='hero_image_vector' src={heroImageVector} alt='affordable beautiful homes' />
+                    </div>
+                </header>
+                <article id='search'>
+                    <div className='search_container'>
+                        <div>
+                            <select id='location' name='location'>
+                                <option>Location</option>
+                                <option>Jahi</option>
+                                <option>Maitama</option>
+                            </select>
+                        </div>
+                        <div>
+                            <select id='type' name='type'>
+                                <option>Property Type</option>
+                                <option>Duplex</option>
+                                <option>Bungalow</option>
+                            </select>
+                        </div>
+                        <div>
+                            <select id='price' name='price'>
+                                <option>Max Price</option>
+                                <option>$20,000</option>
+                                <option>$200,000</option>
+                                <option>$2,000,000</option>
+                            </select>
+                        </div>
+                        <button>
+                            <img src={search} alt='search icon' />
+                            <span>Search</span>
                         </button>
                     </div>
-                </div>
-                <div className='hero'>
-                    <img className='hero_image' src={heroImage} alt='affordable beautiful homes' />
-                    <img className='hero_image_vector' src={heroImageVector} alt='affordable beautiful homes' />
-                </div>
-            </header>
-            <article id='search'>
-                <div className='search_container'>
-                    <div>
-                        <select id='location' name='location'>
-                            <option>Location</option>
-                            <option>Jahi</option>
-                            <option>Maitama</option>
-                        </select>
-                    </div>
-                    <div>
-                        <select id='type' name='type'>
-                            <option>Property Type</option>
-                            <option>Duplex</option>
-                            <option>Bungalow</option>
-                        </select>
-                    </div>
-                    <div>
-                        <select id='price' name='price'>
-                            <option>Max Price</option>
-                            <option>$20,000</option>
-                            <option>200,000</option>
-                        </select>
-                    </div>
-                    <button>
-                        <img src={search} alt='search icon' />
-                        <span>Search</span>
-                    </button>
-                </div>
-            </article>
-            <main>
+                </article>
                 <section className='content'>
                     <article id='how_it_works'>
                         <h2>How it works.</h2>
@@ -99,6 +105,63 @@ function App() {
                                 <img src={secureImg} alt='key handover' />
                                 <h3>Make it Official</h3>
                                 <p>We have been in business for over 32 years, for client outside the country you can trust  us to deliver well.</p>
+                            </div>
+                        </div>
+                    </article>
+                    <article id='why_us'>
+                        <div className='points'>
+                            <h2>Why you should choose us.</h2>
+                            <p>Creating quality urban lifestyles, building stronger communities.</p>
+                            <ul>
+                                <li><img src={checkMark} alt='check mark' />World class</li>
+                                <li><img src={checkMark} alt='check mark' />Affordable</li>
+                                <li><img src={checkMark} alt='check mark' />Trusted</li>
+                                <li><img src={checkMark} alt='check mark' />Amenities</li>
+                            </ul>
+                        </div>
+                        <div className={'why_us_image'}>
+                            <img src={whyUs} alt='beautiful bungalow in clear day sky' />
+                        </div>
+                    </article>
+                    <article id='featured_properties'>
+                        <h2>Featured Properties</h2>
+                        <p>A few of our biggest products that have sold out the most.</p>
+                        <div className='cards'>
+                            <div className='card'>
+                                <img src={thePalace} alt='a duplex building' />
+                                <div><h3>The Palace</h3><span>Duplex</span></div>
+                                <div><span>Lekki, phase 2</span><span className='price'>$2,000</span></div>
+                            </div>
+                            <div className='card'>
+                                <img src={ivyRory} alt='a duplex building' />
+                                <div><h3>Ivy Rory</h3><span>Duplex</span></div>
+                                <div><span>Lekki, phase 2</span><span className='price'>$2,000</span></div>
+                            </div>
+                            <div className='card'>
+                                <img src={rexona} alt='a duplex building' />
+                                <div><h3>Rexona</h3><span>Duplex</span></div>
+                                <div><span>Lekki, phase 2</span><span className='price'>$2,000</span></div>
+                            </div>
+                        </div>
+                    </article>
+                    <article id='testimonials'>
+                        <h2>Testimonials.</h2>
+                        <p>What our clients are saying</p>
+                        <div className='cards'>
+                            <div className='card'>
+                                <h3>Alex Godwin</h3>
+                                <span>South Africa</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fames ut dictumst urna, lorem nibh. Pretium leo hendrerit interdum netus</p>
+                            </div>
+                            <div className='card'>
+                                <h3>Alex Godwin</h3>
+                                <span>South Africa</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fames ut dictumst urna, lorem nibh. Pretium leo hendrerit interdum netus</p>
+                            </div>
+                            <div className='card'>
+                                <h3>Alex Godwin</h3>
+                                <span>South Africa</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fames ut dictumst urna, lorem nibh. Pretium leo hendrerit interdum netus</p>
                             </div>
                         </div>
                     </article>
